93c406e7b0e9926806c9970fe3590ef5f74c797b
kaung khant <kkhantthu97@gmail.com>
2021-10-06 14:41:35 +1100

    resolve file name change for outcome

--- app/src/main/java/kkt/sai/composetodoapp/model/DefaultLocalDataSource.kt
+++ app/src/main/java/kkt/sai/composetodoapp/model/DefaultLocalDataSource.kt
@@ -1,15 +1,16 @@
 package kkt.sai.composetodoapp.model
 
+import kkt.sai.composetodoapp.entity.OutCome
 import kkt.sai.composetodoapp.entity.Task
 
 class DefaultLocalDataSource : LocalDataSource{
 
-    override fun getTasks(): Result<List<Task>> {
+    override fun getTasks(): OutCome<List<Task>> {
 
         val sampleData = ArrayList<Task>();
         sampleData.add(Task("gg","detail",1));
 
-        return Result.success(sampleData);
+        return OutCome.Success(sampleData);
 
     }
 }
